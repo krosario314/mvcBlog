c918dc74cd1ab6438e1441ae1b8b020fb024b4e5
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 18:58:23 -0400

    defining table columns such as id , username, email and password

--- models/user.js
+++ models/user.js
@@ -7,3 +7,43 @@ class User extends Model {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
+User.init(
+    {
+        // id
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        // username
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        twitter: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        github: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        // email
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true
+          }
+        },
+        // password
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [4]
+          }
+        }
+      },
