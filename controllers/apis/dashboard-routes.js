593ef027bce766c1d1c8e54751433572c4917d57
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 17:11:01 -0400

    find one get router database post if msg finds no id return 404, serializing data, post database true render post

--- controllers/apis/dashboard-routes.js
+++ controllers/apis/dashboard-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
-
+// find all
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
       where: {
@@ -30,7 +30,6 @@ router.get('/', withAuth, (req, res) => {
       ]
     })
       .then(dbPostData => {
-        // serialize data before passing to template
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('dashboard', { posts, loggedIn: true });
       })
@@ -39,3 +38,48 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
       });
   });
+  // find one
+  router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        'post_content'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username', 'twitter', 'github']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username', 'twitter', 'github']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        const post = dbPostData.get({ plain: true });
+
+        res.render('edit-post', {
+            post,
+            loggedIn: true
+            });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
