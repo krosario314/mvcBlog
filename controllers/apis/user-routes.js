e4a0d2c5fb67b74e162952cd7b312f3d5bc2588e
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 18:42:39 -0400

    loggin in must have valid pw if incorrect return 400stat , session variables

--- controllers/apis/user-routes.js
+++ controllers/apis/user-routes.js
@@ -68,3 +68,33 @@ router.post('/', (req, res) => {
       });
     });
   });
+
+  router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.twitter = dbUserData.twitter;
+        req.session.github = dbUserData.github;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+  });
