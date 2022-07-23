ea237b3e05b49d302ee46394f26806e9d45f3a73
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 18:43:50 -0400

    putting api users 1

--- controllers/apis/user-routes.js
+++ controllers/apis/user-routes.js
@@ -98,3 +98,35 @@ router.post('/', (req, res) => {
       });
     });
   });
+
+
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
+
+router.put('/:id', withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
