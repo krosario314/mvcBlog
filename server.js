c1f7ac1ed70b41aba844bf8d152f5e0113946e2a
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 16:27:10 -0400

    turn on connection to db and server

--- server.js
+++ server.js
@@ -28,3 +28,17 @@ const sess = {
     db: sequelize
   }),
 };
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
