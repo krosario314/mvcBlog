b3f27a4995e95c0da89208d01c51b2d335090e9b
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 18:45:30 -0400

    deleting api users 1 exporting ruter

--- controllers/apis/user-routes.js
+++ controllers/apis/user-routes.js
@@ -130,3 +130,24 @@ router.put('/:id', withAuth, (req, res) => {
         res.status(500).json(err);
       });
   });
+
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
