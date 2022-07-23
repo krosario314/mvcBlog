090f40e6f58f43cce37ed4b7e4a9ee3ae072fa80
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 16:20:17 -0400

    adding applications express, route controllwra, sequelize in path to config connection, path, requiring helpers, handlebars under utils, creates helpers env port or lh3001

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
