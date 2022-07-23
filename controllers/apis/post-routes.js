cf76df40054732dc3400ee4037b31736d8b55be5
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 18:35:21 -0400

    route put post update if no post for id found , router delete post if no post has relating id return database post exporting router

--- controllers/apis/post-routes.js
+++ controllers/apis/post-routes.js
@@ -84,3 +84,46 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
 });
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        post_content: req.body.post_content
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
