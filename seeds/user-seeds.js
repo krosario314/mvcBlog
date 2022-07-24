1823833ced579efbf61efce5557cd5c9f701f3be
Kayla Rosario <rosariok314@gmail.com>
2022-07-23 20:50:13 -0400

    seeds folder added new files called comments seeds, index seeds, post seeds, user seeds, user john jane kayla colson jennalyn gracie, incluing password for all user data, post seeds 3 text area with title and contents of body, why mvc, authen and author, ORM, index.js to include checking all seeds, seed users, seeds posts, seed comments, exit, comment seeds user 1, 4, 3 comments

---
+++ seeds/user-seeds.js
@@ -0,0 +1,50 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: "john_doe",
+        twitter: "jDoe",
+        github: "jDoe",
+        email: "jDoe@gmail.com",
+        password: "password1"
+    },
+    {
+        username: "Jane_doe",
+        twitter: "janedoe_",
+        github: "jadoe",
+        email: "jane_doe@gmail.com",
+        password: "password2"
+    },
+    {
+        username: "kayla_r",
+        twitter: "kayla_r",
+        github: "kaylar",
+        email: "kayla__r@gmail.com",
+        password: "password3"
+    },
+    {
+        username: "b_colson",
+        twitter: "colson_b",
+        github: "bakerC",
+        email: "colson.baker@gmail.com",
+        password: "password4"
+    },
+    {
+        username: "JennalynGee",
+        twitter: "JennalynGee",
+        github: "JennalynGee",
+        email: "j_gee@gmail.com",
+        password: "password5"
+    },
+    {
+        username: "Gracie_mach",
+        twitter: "Gracie_mach",
+        github: "Gracie_mach",
+        email: "graciemach6@gmail.com",
+        password: "password6"
+    }
+]
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
